b0083fc4268924f375c6b6385c80c5bbe279bd9d
Dustin Schau <dustinschau@gmail.com>
2019-03-04 17:51:57 -0600

    feat: add images

---
+++ src/components/hero.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import Image from 'gatsby-image'
+
+import styles from './hero.module.css'
+
+export default function Hero() {
+  const images = useStaticQuery(graphql`
+    {
+      mobile: file(relativePath:{
+        regex:"/phone.jpg/"
+      }) {
+        childImageSharp {
+          fluid(maxWidth:500) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      desktop: file(relativePath:{
+        regex:"/desktop.jpg/"
+      }) {
+        childImageSharp {
+          fluid(maxWidth:500) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <div className={styles.hero}>
+      <Image className={[styles.heroImage].concat(styles.mobile).join(' ')} fluid={images.mobile.childImageSharp.fluid} />
+      <Image className={[styles.heroImage].concat(styles.desktop).join(' ')} fluid={images.desktop.childImageSharp.fluid} />
+    </div>
+  )
+}
